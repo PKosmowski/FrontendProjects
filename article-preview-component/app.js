d03064e4a49823deaa2abb5c964cb8cc5ae5cbbe
Piotr Kosmowski <pkosmowski553@gmail.com>
2024-12-11 16:55:34 +0100

    finished

--- article-preview-component/app.js
+++ article-preview-component/app.js
@@ -1,10 +1,11 @@
 const shareButton = document.getElementById('shareButton')
 const shareOptions = document.getElementById('shareOptions')
-shareOptions.style.visibility='hidden'
+shareOptions.style.visibility = 'hidden'
 
 shareButton.onclick = () => {
     console.log('witam');
     shareButton.style.backgroundColor = "hsl(217, 19%, 35%)"
     shareOptions.style.visibility = 'visible'
     
+
 }
