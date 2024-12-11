f55f1e8b897fd1e8114219f17ed11f320e1480b4
Piotr Kosmowski <pkosmowski553@gmail.com>
2024-12-11 11:39:12 +0100

    wip app.js

---
+++ article-preview-component/app.js
@@ -0,0 +1,7 @@
+const shareButton = document.getElementById('share')
+
+shareButton.onclick = () => {
+    console.log('witam');
+    shareButton.style.backgroundColor = "hsl(217, 19%, 35%)"
+    
+}
