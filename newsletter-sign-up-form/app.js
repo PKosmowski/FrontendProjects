172d49aaceb70dc19a45cc36b829fe2bd6b899fb
Piotr Kosmowski <pkosmowski553@gmail.com>
2024-12-12 01:41:45 +0100

    app.js wip

---
+++ newsletter-sign-up-form/app.js
@@ -0,0 +1,26 @@
+const button = document.getElementById("subscribe")
+const email = document.getElementById("emailInput").value
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+
+button.onclick = () => {
+    const email = document.getElementById("emailInput").value
+    let main = document.querySelector('start')
+    console.log(email);
+    if (validateEmail(email)) {
+        console.log('holy!');
+        main.className = 'submitted'
+
+        
+    } else {
+        console.log('dziala - niepoprawne');
+        
+    }
+    
+}
