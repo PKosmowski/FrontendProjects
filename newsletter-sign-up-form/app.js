7b540bee1a5072f9df525fd3854ab3abc48065ea
Piotr Kosmowski <pkosmowski553@gmail.com>
2024-12-12 21:58:29 +0100

    finished (not working 100%)

--- newsletter-sign-up-form/app.js
+++ newsletter-sign-up-form/app.js
@@ -1,25 +1,59 @@
-const button = document.getElementById("subscribe")
-const email = document.getElementById("emailInput").value
-const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
+// const button = document.getElementById("subscribe")
+
+// const submitContainer = document.querySelectorAll("container-submit")
 
 
-button.onclick = () => {
-    const email = document.getElementById("emailInput").value
-    let main = document.getElementsByClassName('start')
-    if (validateEmail(email)) {
-        console.log('holy!');
-        main.className = 'submitted'
 
-        
-    } else {
-        console.log('dziala - niepoprawne');
-        
-    }
+// button.onclick = () => {
+//   const email = document.getElementById("emailInput").value
+//   const submitContainer = document.querySelectorAll("container-submit")
+//   if (validateEmail(email)) {
+//     console.log('witam');
+//     changeVisibility()
     
-}
+//   } else {
+//       console.log('dziala - niepoprawne');   
+//   }
+// }
+
+// function changeVisibility() {
+//   document.getElementsByClassName('container-submit').style.visibility = "hidden"
+// }
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const subscribeButton = document.getElementById("subscribe");
+  const submitContainer = document.querySelector(".container-submit");
+  const body = document.body;
+  const test = document.querySelector(".text")
+  const img = document.getElementById("illustration")
+  const email = document.getElementById("emailInput").value
+
+  subscribeButton.addEventListener("click", () => {
+    // Dodaj klasę `hidden` do całego body
+    const email = document.getElementById("emailInput").value
+    if (validateEmail(email)) {
+      submitContainer.style.display = "flex";
+      test.style.display = "none";
+    }
+ 
+
+    // Pokaż tylko container-submit
+
+  });
+
+  const dismissButton = document.getElementById("dismiss");
+  dismissButton.addEventListener("click", () => {
+    // Usuń klasę `hidden` i schowaj submit-container
+    test.style.display = "flex";
+    submitContainer.style.display = "none";
+  });
+});
